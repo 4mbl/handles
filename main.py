949fb9c062cdf90e9a945a84a49e6a221f019d76
4MBL <74970679+4MBL@users.noreply.github.com>
2023-05-16 17:06:40 +0300

    feat: create main file

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import github
+
+
+def process_input(input_dir: str = os.path.join(os.path.dirname(__file__),
+                                                'secret')):
+
+    usernames = []
+    for file_name in os.listdir(input_dir):
+        file_path = os.path.join(input_dir, file_name)
+        with open(file_path, encoding='UTF-8') as f:
+            for line in f:
+                if line == "\n":
+                    continue
+                if file_name.endswith('.txt'):
+                    usernames.append(line.strip())
+                elif file_name.endswith('.csv'):
+                    line = line.split(';')
+                    usernames.append(line[0].rstrip())
+
+    return usernames
+
+
+def main():
+    usernames = process_input()
+    print(github.are_available(usernames))
+
+
+if __name__ == '__main__':
+    main()
