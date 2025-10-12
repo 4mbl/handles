27a8bccc22a632ed7d6455d3d7e040a007c99c91
4MBL <74970679+4mbl@users.noreply.github.com>
2025-10-12 19:39:18 +0300

    add support for bluesky

---
+++ src/handles/platforms/bluesky.py
@@ -0,0 +1,15 @@
+from http.client import OK
+
+import requests  # type: ignore[import]
+
+from handles.platform import Platform
+
+
+class Bluesky(Platform):
+    def is_available(self, username: str) -> bool:
+        handle = username.strip()
+        handle = handle + '.bsky.social'
+        handle = handle.lower()
+        url = f'https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle={handle}'
+        response = requests.get(url, timeout=10)
+        return response.status_code != OK
